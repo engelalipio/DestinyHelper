f945c41915825058bd1fa01adf9016c0502bbc4d
Engel Alipio <engel@agilemobilesolutions.org>
2021-04-01 15:25:17 -0400

    New File

---
+++ Models/Memberships/MBRDataModels.h
@@ -0,0 +1,16 @@
+//
+//  DataModels.h
+//
+//  Created by Engel Alipio on 4/29/17
+//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
+//
+
+
+#import "NSObject+JTObjectMapping.h"
+#import "AbstractModel.h"
+
+#import "MBRResponse.h"
+#import "MBRBungieNetUser.h"
+#import "MBRDestinyMemberships.h"
+#import "MBRMessageData.h"
+#import "MBRBaseClass.h"
