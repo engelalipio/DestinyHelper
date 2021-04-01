e3dc06acdd0bec5de9721fcf1987682687f25d5f
Engel Alipio <engel@agilemobilesolutions.org>
2021-04-01 15:25:00 -0400

    New File

---
+++ Models/GuardianClass/GLSDataModels.h
@@ -0,0 +1,16 @@
+//
+//  DataModels.h
+//
+//  Created by Engel Alipio on 4/29/17
+//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
+//
+
+ 
+#import "NSObject+JTObjectMapping.h"
+#import "AbstractModel.h"
+
+#import "GLSResponse.h"
+#import "GLSData.h"
+#import "GLSMessageData.h"
+#import "GLSClassDefinition.h"
+#import "GLSBaseClass.h"
