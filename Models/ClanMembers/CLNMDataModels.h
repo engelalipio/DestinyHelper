5184122ad19354f22b3bc516e4ab5c5051d7cf84
Engel Alipio <engel@agilemobilesolutions.org>
2021-04-01 15:24:21 -0400

    New File

---
+++ Models/ClanMembers/CLNMDataModels.h
@@ -0,0 +1,14 @@
+//
+//  CLNMDataModels.h
+//
+//  Created by Engel Alipio on 3/4/21
+//  Copyright (c) 2021 Citi. All rights reserved.
+//
+
+#import "CLNMMessageData.h"
+#import "CLNMBungieNetUserInfo.h"
+#import "CLNMDestinyUserInfo.h"
+#import "CLNMResponse.h"
+#import "Query.h"
+#import "Results.h"
+#import "CLNMBaseClass.h"
